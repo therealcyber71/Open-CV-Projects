bbcb4233092ba4af997f487c5d9ecb80a73f8bbc
Sachit <72592470+Sachit71@users.noreply.github.com>
2021-08-30 12:46:53 +0530

    Update visualtrans.py

--- OCR based translator using open-cv/visualtrans.py
+++ OCR based translator using open-cv/visualtrans.py
@@ -1,7 +1,7 @@
 import pytesseract as tess
 from deep_translator import GoogleTranslator
 import cv2
-tess.pytesseract.tesseract_cmd = r'C:\Users\Sachit Ramesh Gowda\Desktop\tess\tesseract.exe'
+tess.pytesseract.tesseract_cmd = r'path to tesseract.exe'
 from PIL import Image
 
 img_cv = cv2.imread('text.png')
